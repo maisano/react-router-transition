c8180449c0886865c89c14987936f5ee189bedde
Richard Maisano <rmaisano@squarespace.com>
2016-02-10 10:06:38 -0500

    clean up some of the demo code.

--- demos/Lorem.jsx
+++ demos/Lorem.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+/* eslint-disable */
 const lorems = `Lorem ipsum dolor sit amet, hinc wisi facilisis sit ea. Eam prima nostro eu, dicit tritani ea vis. In propriae signiferumque vim. Nostro accusata sed no, iisque scripserit te ius. His utamur incorrupte te. Ceteros euripidis consectetuer mea ad, id mea adhuc ludus voluptatibus.
 Exerci mollis delicata eum no, cum id commodo interpretaris. Ad soleat pertinacia mea. Nihil nominavi complectitur id sea. Vis docendi ponderum cu, vocent accommodare consequuntur duo et, alienum sapientem adversarium his te. Ius in nonumy reformidans.
 Moderatius dissentiet nec ne. Usu no stet voluptatum moderatius, sed magna utinam an, in has dolore latine indoctum. Malorum eloquentiam ex quo, te cetero signiferumque nec. Ei usu populo labores urbanitas, usu et dico idque conclusionemque, vivendum persequeris disputationi nam eu.
 At vis persequeris mediocritatem, quas omnes tantas in nam, iuvaret officiis te vel. Velit munere per an, graece malorum repudiare mei id, everti repudiare ei eum. Eum id nibh dignissim. At sed tota tation adversarium, in veniam vivendum dissentiunt vim. Pro ad discere nominati disputando.
 Populo mentitum has et, nobis populo et nam. Minimum erroribus prodesset ad qui. Eum utroque mediocrem ut, ut placerat inimicus deseruisse eam. Ubique consulatu et per, hinc agam tollit mel an. Per quem omnesque lucilius in, mei dicta ancillae ei. Pro te summo hendrerit.`.split('\n');
+/* eslint-enable */
 
 let count = 0;
 
--- demos/RouteTransitionDemo.jsx
+++ demos/RouteTransitionDemo.jsx
@@ -3,27 +3,25 @@ import { Link } from 'react-router';
 
 import { RouteTransition } from '../src/index';
 
-const RouteTransitionDemo = (props) => {
-  return (
-    <div>
-      {props.route.childRoutes.map((childRoute, index) => {
-        const to = ['', props.route.path, childRoute.path].join('/');
-        return (
-          <Link className="child-route" key={to} to={to}>
-            {props.route.path} {index}
-          </Link>
-        );
-      })}
-      <RouteTransition
-        component={false}
-        className="transition-wrapper"
-        pathname={props.location.pathname}
-        {...props.preset}
-      >
-        {props.children}
-      </RouteTransition>
-    </div>
-  );
-};
+const RouteTransitionDemo = (props) => (
+  <div>
+    {props.route.childRoutes.map((childRoute, index) => {
+      const to = ['', props.route.path, childRoute.path].join('/');
+      return (
+        <Link className="link" key={to} to={to}>
+          {props.route.path} {index}
+        </Link>
+      );
+    })}
+    <RouteTransition
+      component={false}
+      className="transition-wrapper"
+      pathname={props.location.pathname}
+      {...props.preset}
+    >
+      {props.children}
+    </RouteTransition>
+  </div>
+);
 
 export default RouteTransitionDemo;
