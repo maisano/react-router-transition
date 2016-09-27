613b32225a78ff658aa574e22a498aa2971348ac
zjlovezj <zjlovezj@gmail.com>
2016-09-27 15:47:45 +0800

    import from react-motion/lib to reduce bundle size
    This change could make the lib/react-router-transition.js reduce ~20KB

--- src/ensureSpring.js
+++ src/ensureSpring.js
@@ -1,4 +1,4 @@
-import { spring } from 'react-motion';
+import spring from 'react-motion/lib/spring';
 
 export default function ensureSpring(styles) {
   return Object.keys(styles).reduce((acc, key) => {
