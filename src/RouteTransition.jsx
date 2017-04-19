ef9ff04e02906b15853a8246fa1f47b7fcb179a2
Mickael Lecoq <m.blacksad@gmail.com>
2017-04-19 23:35:56 +0200

    review binding & run prettier

--- src/RouteTransition.jsx
+++ src/RouteTransition.jsx
@@ -1,11 +1,10 @@
-import React, { cloneElement, createElement, Component } from 'react';
-import TransitionMotion from 'react-motion/lib/TransitionMotion';
+import React, { cloneElement, createElement, Component } from "react";
+import TransitionMotion from "react-motion/lib/TransitionMotion";
 import PropTypes from "prop-types";
 
-import ensureSpring from './ensureSpring';
+import ensureSpring from "./ensureSpring";
 
-class RouteTransition extends Component{
-
+class RouteTransition extends Component {
   constructor(props) {
     super(props);
   }
@@ -19,11 +18,13 @@ class RouteTransition extends Component{
       return [];
     }
 
-    return [{
-      key: this.props.pathname,
-      data: this.props.children,
-      style: this.props.atEnter
-    }];
+    return [
+      {
+        key: this.props.pathname,
+        data: this.props.children,
+        style: this.props.atEnter
+      }
+    ];
   }
 
   // there's only ever one route mounted at a time,
@@ -34,19 +35,21 @@ class RouteTransition extends Component{
     }
 
     // TODO: maybe access route path from children for pathname?
-    return [{
-      key: this.props.pathname,
-      data: this.props.children,
-      style: ensureSpring(this.props.atActive)
-    }];
+    return [
+      {
+        key: this.props.pathname,
+        data: this.props.children,
+        style: ensureSpring(this.props.atActive)
+      }
+    ];
   }
 
   willEnter() {
-    return this.props.atEnter;
+    return () => this.props.atEnter;
   }
 
   willLeave() {
-    return ensureSpring(this.props.atLeave);
+    return () => ensureSpring(this.props.atLeave);
   }
 
   renderRoute(config) {
@@ -63,7 +66,7 @@ class RouteTransition extends Component{
   renderRoutes(interpolatedStyles) {
     return (
       <div className={this.props.className} style={this.props.style}>
-        {interpolatedStyles.map(this.renderRoute.bind(this))}
+        {interpolatedStyles.map(config => this.renderRoute(config))}
       </div>
     );
   }
@@ -73,27 +76,24 @@ class RouteTransition extends Component{
       <TransitionMotion
         defaultStyles={this.getDefaultStyles()}
         styles={this.getStyles()}
-        willEnter={this.willEnter.bind(this)}
-        willLeave={this.willLeave.bind(this)}
+        willEnter={this.willEnter()}
+        willLeave={this.willLeave()}
       >
-        {interpolatingStyle => this.renderRoutes(interpolatingStyle)}
+        {interpolatedStyles => this.renderRoutes(interpolatedStyles)}
       </TransitionMotion>
     );
   }
-};
+}
 
 RouteTransition.defaultProps = {
-  component: 'div',
+  component: "div",
   runOnMount: true,
   mapStyles: val => val
 };
 
 RouteTransition.propTypes = {
   className: PropTypes.string,
-  component: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.bool
-  ]),
+  component: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
   pathname: PropTypes.string.isRequired,
   atEnter: PropTypes.object.isRequired,
   atActive: PropTypes.object.isRequired,
