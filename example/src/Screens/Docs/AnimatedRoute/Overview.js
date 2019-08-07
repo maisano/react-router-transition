3b57cc2edba3c0b459a4c24a2115d1d7dae70a9b
Austin Jackson <themacphage@gmail.com>
2019-08-07 12:46:03 -0500

    Fix typo in Overview.js

--- example/src/Screens/Docs/AnimatedRoute/Overview.js
+++ example/src/Screens/Docs/AnimatedRoute/Overview.js
@@ -12,7 +12,7 @@ const Overview = () => (
     <h2>Overview</h2>
     <p>A <a href="https://reacttraining.com/react-router/web/api/Route"><Element name="Route" /></a>, but with mounting &amp; unmounting transitions.</p>
     <h2>Interruptible</h2>
-    <p>Transitions on an <Element name="AnimateRoute" /> are <em>interruptible</em>. This means that if an animation is currently in motion and its match is toggled, it will redirect itself to its proper state mid-transition.</p>
+    <p>Transitions on an <Element name="AnimatedRoute" /> are <em>interruptible</em>. This means that if an animation is currently in motion and its match is toggled, it will redirect itself to its proper state mid-transition.</p>
     <h2>Nestable</h2>
     <p>You can put <Element name="AnimatedRoute" /> instances inside one another! You can even make them recursively nest and match–it's very likely not useful, but it sure is interesting.</p>
     <Footer />
